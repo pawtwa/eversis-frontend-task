756b3e1372faefffe2176243044860e8ca7a9227
Paweł Twardziak <ptwardziak@unity-t.pl>
2019-04-02 14:49:29 +0200

    Run Header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { EditComponent } from './user/edit/edit.component';
-import { ViewComponent } from './user/view/view.component';
-import { HomeComponent } from './home/home.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    EditComponent,
-    ViewComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    CollapseModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+// import { CommonModule } from '@angular/common';
+
+import { HeaderComponent } from '../header/header.component';
+import { HomeComponent } from '../home/home.component';
+import { UserService } from './services/user.service';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    HomeComponent
+  ],
   imports: [
-    CommonModule
+    // CommonModule
+  ],
+  exports: [
+    HeaderComponent,
+    HomeComponent
+  ],
+  providers: [
+    UserService
   ]
 })
 export class SharedModule { }
